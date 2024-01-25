f7e84aa40d197a04ab88503101519ebe0adb4ade
Bobilor <122546923+Bobilor@users.noreply.github.com>
2024-01-25 14:00:40 -0500

    Add files via upload

--- January-23.R
+++ January-23.R
@@ -11,3 +11,29 @@ data<-ReadAffy()
 
 #Visualize data
 boxplot(data)
+
+
+#Perform RMA normalization
+data_norm <-rma(data)
+
+#Extract the expression data from normalized data variable
+data_norm_exprs<-exprs(data_norm)
+
+
+#Plot the normalized dataset
+boxplot(data_norm_exprs)
+
+
+#Group into treatment and controls for fold change analysis
+Control<-data_norm_exprs[,c(1:2)]
+Treatment<-data_norm_exprs[,c(3:4)]
+
+#Take averages for treatment and controls for fold change analysis
+Control<-rowMeans(Control)
+Treatment<-rowMeans(Treatment)
+
+#Take the fold Change
+Fold_change<-Treatment-Control
+
+
+
